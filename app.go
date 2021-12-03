a067f0ee4c524f380f65ff4b5cb04aff9298d6dd
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-12-03 18:49:47 +0100

    fix: rename WHOAMI_PORT to WHOAMI_PORT_NUMBER
    k8s creates env var based on service name,
    then WHOAMI_PORT exists and contains a URL instead of a number.

--- app.go
+++ app.go
@@ -43,7 +43,7 @@ func init() {
 	flag.StringVar(&cert, "cert", "", "give me a certificate")
 	flag.StringVar(&key, "key", "", "give me a key")
 	flag.StringVar(&ca, "cacert", "", "give me a CA chain, enforces mutual TLS")
-	flag.StringVar(&port, "port", getEnv("WHOAMI_PORT", "80"), "give me a port number")
+	flag.StringVar(&port, "port", getEnv("WHOAMI_PORT_NUMBER", "80"), "give me a port number")
 	flag.StringVar(&name, "name", os.Getenv("WHOAMI_NAME"), "give me a name")
 }
 
