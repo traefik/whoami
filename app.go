056cddd93ad710d9ed7845637541707f8bc3e89a
Fernandez Ludovic <ldez@users.noreply.github.com>
2023-02-17 10:11:18 +0100

    review

--- app.go
+++ app.go
@@ -51,8 +51,8 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-        	return true
-	},	
+		return true
+	},
 }
 
 func main() {
