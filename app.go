30767b10c576c03c208deedb3c6d56621599cb8e
iesrbt <ies.rbt@gmail.com>
2023-02-17 10:18:14 +0100

    added WebSocket origin check

--- app.go
+++ app.go
@@ -50,6 +50,9 @@ func init() {
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func main() {
